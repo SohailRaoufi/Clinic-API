d064298f6521c54d469e22e79a0ed997bc7a9bf1
Ahmad Sohail Raoufi <sohailraufi19@gmail.com>
2024-10-07 18:40:46 +0430

    Staff Added

--- core/urls.py
+++ core/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.urls import include, path
-from user.views import JwtToken
+from user.views import JwtToken, Staff
 from rest_framework import routers
-from patient.views import AppointmentViewSet, PatientView,TreatmentViewSet 
+from patient.views import AppointmentViewSet, PatientView, TreatmentViewSet
 
 
 router = routers.DefaultRouter()
-router.register("patient",PatientView)
-router.register("treatment",TreatmentViewSet)
-router.register("appointment",AppointmentViewSet)
+router.register("patient", PatientView)
+router.register("treatment", TreatmentViewSet)
+router.register("appointment", AppointmentViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/",include(router.urls)),
-    path("api/auth/",JwtToken.as_view())
+    path("api/", include(router.urls)),
+    path("api/auth/", JwtToken.as_view()),
+    path("api/auth/create", Staff.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -1,32 +1,54 @@
+from typing import Any
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from .token_factory import create_token
-
+from django.contrib.auth.models import UserManager
 
 
 class JwtToken(APIView):
-    def post(self,request):
-        email = request.data.get("username",None)
-        password = request.data.get("password",None)
+    def post(self, request):
+        email = request.data.get("username", None)
+        password = request.data.get("password", None)
         if not email or not password:
             return Response(
-                {"detail" : "'username' and 'password' required"},
+                {"detail": "'username' and 'password' required"},
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        user = authenticate(username=email,password=password)
+        user = authenticate(username=email, password=password)
         if user and user.is_active:
             token = create_token(user)
             return Response(
                 {
-                    "token" : token,
+                    "token": token,
                 },
                 status=status.HTTP_200_OK
             )
         else:
             return Response(
-                {"detail" : "Invalid User"},
+                {"detail": "Invalid User"},
                 status=status.HTTP_400_BAD_REQUEST
             )
+
+
+class Staff(APIView, UserManager):
+    def create_user(self, username: str, email: str | None = ..., password: str | None = ..., **extra_fields: Any) -> Any:
+        return super().create_user(username, email, password, **extra_fields)
+
+    def post(self, request):
+        username = request.data.get("username", None)
+        email = request.data.get("email", None)
+        password = request.data.get("password", None)
+
+        if not username or not password:
+            return Response(
+                {"detail": "'username' and 'password' required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        self.create_user(username, email, password)
+        return Response(
+            status=status.HTTP_200_OK
+        )
