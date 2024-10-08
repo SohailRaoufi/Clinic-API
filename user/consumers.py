cf44ede64eb991adc19e0274655e2dedd8bf4b96
Ahmad Sohail Raoufi <sohailraufi19@gmail.com>
2024-10-08 19:30:26 +0430

    Delete Print Statemetn in Consumer

--- user/consumers.py
+++ user/consumers.py
@@ -6,21 +6,23 @@ from .models import Messages, Room
 from django.contrib.auth.models import User
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
-from .serializers import  MsgSerializer
+from .serializers import MsgSerializer
 from django.contrib.auth import get_user_model
 from django.db.models import Q
 from django.core.files.base import ContentFile
 import base64
 
+
 @sync_to_async
 def get_msgs(room: Room):
-    messages = Messages.objects.filter(room=room) #type:ignore
+    messages = Messages.objects.filter(room=room)  # type:ignore
     serializer = MsgSerializer(messages, many=True)
     return serializer.data
 
+
 @sync_to_async
 def create_msg(room: Room, sender: User, receiver: User, msg: str):
-    msg = Messages.objects.create( #type:ignore
+    msg = Messages.objects.create(  # type:ignore
         sender=sender,
         room=room,
         receiver=receiver,
@@ -30,36 +32,40 @@ def create_msg(room: Room, sender: User, receiver: User, msg: str):
     serializer = MsgSerializer(msg)
     return serializer.data
 
+
 @sync_to_async
 def create_document(room: Room, sender: User, receiver: User, document: Any, file_name: str):
     file_bytes = base64.b64decode(document)
     file = ContentFile(file_bytes, name=file_name)
-    msg = Messages.objects.create( #type:ignore
+    msg = Messages.objects.create(  # type:ignore
         sender=sender,
         room=room,
         receiver=receiver,
         type="link",
-        link=file 
+        link=file
     )
     serializer = MsgSerializer(msg)
     return serializer.data
+
 
 @sync_to_async
 def get_user(email: str):
     try:
         user = User.objects.get(email=email)
         return user, True
-    except User.DoesNotExist: #type:ignore
+    except User.DoesNotExist:  # type:ignore
         return None, False
+
 
 @sync_to_async
 def get_room(user: User, other_user: User):
-    room, _ = Room.objects.get_or_create( #type:ignore
-            user1__in=[user, other_user],
-            user2__in=[user, other_user],
-            defaults={'user1': user, 'user2': other_user}
-        )
-    return room   
+    room, _ = Room.objects.get_or_create(  # type:ignore
+        user1__in=[user, other_user],
+        user2__in=[user, other_user],
+        defaults={'user1': user, 'user2': other_user}
+    )
+    return room
+
 
 @sync_to_async
 def validate_user(access_token):
@@ -67,8 +73,7 @@ def validate_user(access_token):
     if user:
         return user, True
     else:
-        return None,False
-
+        return None, False
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -78,7 +83,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.other_user = None
         query_params = self.scope['query_string'].decode('utf-8')
         self.access_token = query_params.split('=')[1]
-        print(f"access_token :  {self.access_token}")
 
         user, validation_res = await validate_user(self.access_token)
         print(user)
@@ -87,15 +91,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
             return
 
         self.user = user
-        
-        await self.channel_layer.group_add("chats", self.channel_name) #type:ignore
+
+        # type:ignore
+        await self.channel_layer.group_add("chats", self.channel_name)
         await self.accept()
 
-
-
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_name, self.channel_name) #type:ignore
-    
+        # type:ignore
+        await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -109,18 +112,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
             else:
                 await self.send(json.dumps({"error": "User not found"}))
                 return
-            room = await get_room(self.user, self.other_user) #type:ignore
+            room = await get_room(self.user, self.other_user)  # type:ignore
             self.room_name = f"room_{room.id}"
             self.curr_room = room
             msgs = await get_msgs(self.curr_room)
-            await self.channel_layer.group_add(self.room_name, self.channel_name) #type:ignore
+            # type:ignore
+            await self.channel_layer.group_add(self.room_name, self.channel_name)
             await self.send(json.dumps({"type": "chat_history", "messages": msgs}))
-            return 
+            return
 
         elif type_of_msg == "text":
             text = data["text"]
-            msg = await create_msg(self.curr_room, self.user, self.other_user, text) #type:ignore
-            await self.channel_layer.group_send(# type:ignore
+            # type:ignore
+            msg = await create_msg(self.curr_room, self.user, self.other_user, text)
+            await self.channel_layer.group_send(  # type:ignore
                 self.room_name,
                 {
                     "type": "chat_message",
@@ -134,8 +139,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 return
             file_data = data["file_data"]
             file_name = data["file_name"]
-            msg = await create_document(self.curr_room, self.user, self.other_user, file_data, file_name) #type:ignore
-            await self.channel_layer.group_send( #type:ignore
+            # type:ignore
+            msg = await create_document(self.curr_room, self.user, self.other_user, file_data, file_name)
+            await self.channel_layer.group_send(  # type:ignore
                 self.room_name,
                 {
                     "type": "chat_message",
