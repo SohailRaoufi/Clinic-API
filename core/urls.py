cf6c3b8f200e1c6aa5fe6874298654652d9434f0
Masoom-Wahid <masoom3892@gmail.com>
2024-12-29 22:10:39 +0430

    Fix : registered dental lab

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+
 from .backup import BackupView
 from user.views import AnalyticsAPIView,ChatViewSet, JwtToken, Staff, TaskViewSet
 from rest_framework import routers
-from patient.views import AppointmentViewSet, MedicineViewSet, PatientView, TreatmentViewSet, DailyViewSet
+from patient.views import AppointmentViewSet, DentalLabViewSet, MedicineViewSet, PatientView, TreatmentViewSet, DailyViewSet
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -16,7 +17,7 @@ router.register("chats", ChatViewSet, basename="chats")
 router.register("daily", DailyViewSet, basename="daily")
 router.register("tasks", TaskViewSet)
 router.register("medicine", MedicineViewSet,basename="medicine")
-
+router.register('lab',DentalLabViewSet,basename="dentallab")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
